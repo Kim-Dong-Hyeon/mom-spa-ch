fb129b1bbe5e2704b3518848d84b5ae7a52d5146
geon5595 <geon5595@naver.com>
2024-07-03 21:49:56 +0900

    주문, 취소 버튼 추가

--- MomSpaCh/View/View.swift
+++ MomSpaCh/View/View.swift
@@ -32,6 +32,7 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     self.menuCollectionView.delegate = self
     self.menuCollectionView.register(MenuCollectionViewCell.self, forCellWithReuseIdentifier: "cell")
     
+    setupButtonsStackView()
     setCollectionView()
     setCollectionViewConstraint()
     view.backgroundColor = .white
@@ -158,7 +159,76 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
     payLabel.text = String(sum)
   }
   
-  
+  private func setupButtonsStackView() {
+     let cancelButton = createCancelButton()
+     let orderButton = createOrderButton()
+     
+     let stackView = UIStackView(arrangedSubviews: [cancelButton, orderButton])
+     stackView.axis = .horizontal
+     stackView.alignment = .fill
+     stackView.distribution = .fillEqually
+     stackView.spacing = 10
+     
+     view.addSubview(stackView)
+       
+       stackView.snp.makeConstraints {
+         $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom)
+         $0.leading.equalTo(view.safeAreaLayoutGuide.snp.leading).offset(20)
+         $0.trailing.equalTo(view.safeAreaLayoutGuide.snp.trailing).offset(-20)
+         $0.height.equalTo(50)
+       }
+   }
+   
+   private func createOrderButton() -> UIButton {
+     let button = UIButton(type: .system)
+     button.setTitle("주문하기", for: .normal)
+     button.titleLabel?.font = UIFont(name: "HelveticaNeue-Bold", size: 20)
+     button.backgroundColor = UIColor(red: 235/255, green: 51/255, blue: 77/255, alpha: 1.0)
+     button.setTitleColor(.white, for: .normal)
+     button.layer.cornerRadius = 5
+     
+     button.addTarget(self, action: #selector(orderButtonTapped), for: .touchUpInside)
+     return button
+   }
+   
+   private func createCancelButton() -> UIButton {
+     let button = UIButton(type: .system)
+     button.setTitle("취소", for: .normal)
+     button.titleLabel?.font = UIFont(name: "HelveticaNeue-Bold", size: 20)
+     button.backgroundColor = .white
+     button.setTitleColor(UIColor(red: 235/255, green: 51/255, blue: 77/255, alpha: 1.0), for: .normal)
+     button.layer.borderWidth = 1
+     button.layer.borderColor = UIColor(red: 235/255, green: 51/255, blue: 77/255, alpha: 1.0).cgColor
+     button.layer.cornerRadius = 5
+     
+     //button.addTarget(self, action: #selector(), for: .touchUpInside) 취소 눌렀을 때 동작 추가 예정
+     return button
+   }
+   
+   @objc private func cancelButtonTapped() {
+     // 아니오 눌렀을 때 동작 추가 예정
+   }
+   
+   @objc private func orderButtonTapped() {
+     // 최종 금액 추가 예정
+     let alert = UIAlertController(title: "최종 결제 금액 원입니다. 주문하시겠습니까?", message: "", preferredStyle: .alert)
+     let yesAction = UIAlertAction(title: "네", style: .default) { _ in
+       self.orderCompletedAlert()
+     }
+     let noAction = UIAlertAction(title: "아니오", style: .cancel, handler: nil)
+     alert.addAction(yesAction)
+     alert.addAction(noAction)
+
+     present(alert, animated: true, completion: nil)
+   }
+   
+   private func orderCompletedAlert() {
+     let completedAlert = UIAlertController(title: "주문 완료되었습니다.", message: "", preferredStyle: .alert)
+     let okAction = UIAlertAction(title: "확인", style: .default, handler: nil)
+     completedAlert.addAction(okAction)
+     
+     present(completedAlert, animated: true, completion: nil)
+   }
   
   
   func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
