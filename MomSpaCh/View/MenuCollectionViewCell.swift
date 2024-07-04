f74ae7cd3a8c2e5631c0df96420a260acd91ed22
LikelyLime <qorwlgml2003@naver.com>
2024-07-04 15:53:35 +0900

    Feat : orderList 수정

--- MomSpaCh/View/MenuCollectionViewCell.swift
+++ MomSpaCh/View/MenuCollectionViewCell.swift
@@ -8,8 +8,11 @@
 import UIKit
 
 import SnapKit
-
+protocol MenuCollectionViewCellDelegate: AnyObject {
+  func addOrderList(_: String, _: String)
+}
 class MenuCollectionViewCell: UICollectionViewCell {
+  weak var delegate: MenuCollectionViewCellDelegate?
   lazy var menuData = Data()
   static let identifier = "cell"
   
@@ -75,6 +78,10 @@ class MenuCollectionViewCell: UICollectionViewCell {
   }
   
   @objc func buttonClicked() {
+    var price = priceLabel.text!.filter { $0 != "원" && $0 != " "}
+    var name = nameLabel.text!
+    print(name)
+    delegate?.addOrderList(price, name)
     print("1")
   }
 }
